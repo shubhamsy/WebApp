6b4bc3d9f81de62985bca5e1a3668182f06bb19f
shubhamsy <shubhamyerawar007@gmail.com>
2019-11-18 04:07:06 +0530

    validation changes

--- app/models/slot_booking.rb
+++ app/models/slot_booking.rb
@@ -1,9 +1,9 @@
 class SlotBooking < ApplicationRecord
-    has_many :slots, foreign_key: :slot_booking_id, class_name: "SlotBooking"
+
     has_many :users, foreign_key: :slot_booking_id, class_name: "User"
     
     after_create :send_email    
-    after_create :updateslot
+    # after_create :updateslot
    
     def send_email
     	SlotBookMailer.slot_book(self).deliver

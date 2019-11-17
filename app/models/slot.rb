4fec2461bfe1b5770e9b55757298f330530c06fe
shubhamsy <shubhamyerawar007@gmail.com>
2019-11-18 04:18:01 +0530

    validation changes

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,6 +1,6 @@
 class Slot < ApplicationRecord
   belongs_to :user
-  belongs_to :slot_booking
   validates :start_time, :uniqueness => {:scope => :created_date}
   validates :end_time, :uniqueness => {:scope => :created_date}
+
 end
--- app/models/slot_booking.rb
+++ app/models/slot_booking.rb
@@ -3,15 +3,19 @@ class SlotBooking < ApplicationRecord
     has_many :users, foreign_key: :slot_booking_id, class_name: "User"
     
     after_create :send_email    
-    # after_create :updateslot
+    after_create :updateslot
    
     def send_email
     	SlotBookMailer.slot_book(self).deliver
     end
 
     def updateslot
-       self.slots.each do |slot|
-         slot.update(active: false)
+
+       SlotBooking.all.each do |slot|
+         slot = Slot.find_by(id: slot.id)  
+         if slot.present?
+           slot.update(active: false)
+         end
        end
     end
 

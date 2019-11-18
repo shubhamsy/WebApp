99df7efd209e5a332a6da0d892b88997e22c09ac
shubhamsy <shubhamyerawar007@gmail.com>
2019-11-18 12:48:08 +0530

    change in registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
 
   # GET /resource/sign_up
   def new
